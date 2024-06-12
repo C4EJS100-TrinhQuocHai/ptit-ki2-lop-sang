853a6386fe3b37346f0598503cfc8ed16475640b
trinhquochai <trinhquochai12@gmail.com>
2024-06-12 10:17:02 +0700

    bai tap session58

---
+++ session59/client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Manager from './components/baitap7/Manager'
+
+export default function App() {
+  return (
+    <div>App
+      <p>bài tập 7</p>
+      <Manager></Manager>
+    </div>
+  )
+}
